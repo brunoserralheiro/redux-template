7739c383dac9e24a3cff6fcb9d0554eca1f3e946
brunoserralheiro <brunoserralheiro@users.noreply.github.com>
2020-10-31 17:17:45 +0000

    example react component set state old way

---
+++ src/components/Postform.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export  class Postform extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add Post</h1>         
+            </div>
+        )
+    }
+}
