481e1f59d640a205474ba51137f886d2bb81abdf
brunoserralheiro <brunoserralheiro@users.noreply.github.com>
2020-10-31 22:17:19 +0000

    add redux wip

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {Posts} from "./components/Posts";
+import { Posts } from "./components/Posts";
 import { Postform } from "./components/Postform";
+import { Provider } from "react-redux";
+import store from "./store";
+import rootReducer from "./reducers"
 
 function App() {
+  
   return (
-    <div className="App">
-      <header >
-        <h4>React Redux Template</h4>
-      </header>
-      <Postform />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header>
+          <h4>React Redux Template</h4>
+        </header>
+        <Postform />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
